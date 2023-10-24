a708924a8a0028f498176ed099bd2d9ddf7d2bba
vutuan308 <vutuanfc204@gmail.com>
2023-10-24 07:52:28 +0700

    complite btl

--- main.js
+++ main.js
@@ -14,10 +14,12 @@ document.getElementById("submit").addEventListener("click", function(event) {
       alert("Vui lòng nhập một địa chỉ email hợp lệ.");
       return;
     }
-  
-    document.getElementById("frmcomment").reset();
+
+    alert("Gửi thành công!");
+    document.getElementById("email").value = "";
+    document.getElementById("com").value = "";
+    document.getElementById("rate").value = "";
   });
-  
   function validateEmail(email) {
     var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
